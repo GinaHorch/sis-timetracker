629045334091614ff0b70cae5524307bad5aa18f
Gina <168549659+GinaHorch@users.noreply.github.com>
2025-07-16 18:14:01 +0800

    remove deprecated code

--- src/utils/clients.js
+++
@@ -1,9 +0,0 @@
-const CLIENTS_KEY = 'clients';
-
-export function getClients() {
-  return JSON.parse(localStorage.getItem(CLIENTS_KEY)) || [];
-}
-
-export function saveClients(clients) {
-  localStorage.setItem(CLIENTS_KEY, JSON.stringify(clients));
-}
